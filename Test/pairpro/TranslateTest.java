0bb41ebb66185bff2a6bd589409f98a2b6ed6302
g14943ss <g14943ss@s30431.tsuda.ac.jp>
2016-05-13 11:48:57 +0900

    ドライバーはしおり、ナビゲーターはかんな 30までテストケースをつくりました。 テスト成功しました。

--- Test/pairpro/TranslateTest.java
+++ Test/pairpro/TranslateTest.java
@@ -10,10 +10,184 @@ import pairpro.Translate;
 public class TranslateTest {
 
 	@Test
-	public void 数値を英訳する変換するメソッド(){
+	public void 数字1を英訳する変換するメソッド(){
 		Translate trans = new Translate();
 		int number  = 1;
 		String eng = trans.translateEng(number);
-		assertThat(eng,is(number));
+		assertThat("one",is(eng));
+	}
+	public void 数字2を英訳する変換するメソッド(){
+		Translate trans = new Translate();
+		int number  = 2;
+		String eng = trans.translateEng(number);
+		assertThat("two",is(eng));
+	}
+	public void 数字3を英訳する変換するメソッド(){
+		Translate trans = new Translate();
+		int number  = 3;
+		String eng = trans.translateEng(number);
+		assertThat("three",is(eng));
+	}
+	public void 数字4を英訳する変換するメソッド(){
+		Translate trans = new Translate();
+		int number  = 4;
+		String eng = trans.translateEng(number);
+		assertThat("four",is(eng));
+	}
+	public void 数字5を英訳する変換するメソッド(){
+		Translate trans = new Translate();
+		int number  = 5;
+		String eng = trans.translateEng(number);
+		assertThat("five",is(eng));
+	}
+	public void 数字6を英訳する変換するメソッド(){
+		Translate trans = new Translate();
+		int number  = 6;
+		String eng = trans.translateEng(number);
+		assertThat("six",is(eng));
+	}
+	public void 数字7を英訳する変換するメソッド(){
+		Translate trans = new Translate();
+		int number  = 7;
+		String eng = trans.translateEng(number);
+		assertThat("seven",is(eng));
+	}
+	public void 数字8を英訳する変換するメソッド(){
+		Translate trans = new Translate();
+		int number  = 8;
+		String eng = trans.translateEng(number);
+		assertThat("eight",is(eng));
+	}
+	public void 数字9を英訳する変換するメソッド(){
+		Translate trans = new Translate();
+		int number  = 9;
+		String eng = trans.translateEng(number);
+		assertThat("nine",is(eng));
+	}
+	public void 数字10を英訳する変換するメソッド(){
+		Translate trans = new Translate();
+		int number  = 10;
+		String eng = trans.translateEng(number);
+		assertThat("ten",is(eng));
+	}
+	public void 数字11を英訳する変換するメソッド(){
+		Translate trans = new Translate();
+		int number  = 11;
+		String eng = trans.translateEng(number);
+		assertThat("eleven",is(eng));
+	}
+	public void 数字12を英訳する変換するメソッド(){
+		Translate trans = new Translate();
+		int number  = 12;
+		String eng = trans.translateEng(number);
+		assertThat("twelve",is(eng));
+	}
+	public void 数字13を英訳する変換するメソッド(){
+		Translate trans = new Translate();
+		int number  = 13;
+		String eng = trans.translateEng(number);
+		assertThat("thirteen",is(eng));
+	}
+	public void 数字14を英訳する変換するメソッド(){
+		Translate trans = new Translate();
+		int number  = 14;
+		String eng = trans.translateEng(number);
+		assertThat("fourtenn",is(eng));
+	}
+	public void 数字15を英訳する変換するメソッド(){
+		Translate trans = new Translate();
+		int number  = 15;
+		String eng = trans.translateEng(number);
+		assertThat("fifteen",is(eng));
+	}
+	public void 数字16を英訳する変換するメソッド(){
+		Translate trans = new Translate();
+		int number  = 16;
+		String eng = trans.translateEng(number);
+		assertThat("sixteen",is(eng));
+	}
+	public void 数字17を英訳する変換するメソッド(){
+		Translate trans = new Translate();
+		int number  = 17;
+		String eng = trans.translateEng(number);
+		assertThat("seventeen",is(eng));
+	}
+	public void 数字18を英訳する変換するメソッド(){
+		Translate trans = new Translate();
+		int number  = 18;
+		String eng = trans.translateEng(number);
+		assertThat("eighteen",is(eng));
+	}
+	public void 数字19を英訳する変換するメソッド(){
+		Translate trans = new Translate();
+		int number  = 19;
+		String eng = trans.translateEng(number);
+		assertThat("nineteen",is(eng));
+	}
+	public void 数字20を英訳する変換するメソッド(){
+		Translate trans = new Translate();
+		int number  = 20;
+		String eng = trans.translateEng(number);
+		assertThat("twenty",is(eng));
+	}
+	public void 数字21を英訳する変換するメソッド(){
+		Translate trans = new Translate();
+		int number  = 21;
+		String eng = trans.translateEng(number);
+		assertThat("twenty one",is(eng));
+	}
+	public void 数字22を英訳する変換するメソッド(){
+		Translate trans = new Translate();
+		int number  = 22;
+		String eng = trans.translateEng(number);
+		assertThat("twenty two",is(eng));
+	}
+	public void 数字23を英訳する変換するメソッド(){
+		Translate trans = new Translate();
+		int number  = 23;
+		String eng = trans.translateEng(number);
+		assertThat("twenty three",is(eng));
+	}
+	public void 数字24を英訳する変換するメソッド(){
+		Translate trans = new Translate();
+		int number  = 24;
+		String eng = trans.translateEng(number);
+		assertThat("twenty four",is(eng));
+	}
+	public void 数字25を英訳する変換するメソッド(){
+		Translate trans = new Translate();
+		int number  = 25;
+		String eng = trans.translateEng(number);
+		assertThat("twenty five",is(eng));
+	}
+	public void 数字26を英訳する変換するメソッド(){
+		Translate trans = new Translate();
+		int number  = 26;
+		String eng = trans.translateEng(number);
+		assertThat("twenty six",is(eng));
+	}
+	public void 数字27を英訳する変換するメソッド(){
+		Translate trans = new Translate();
+		int number  = 27;
+		String eng = trans.translateEng(number);
+		assertThat("twenty seven",is(eng));
+	}
+	public void 数字28を英訳する変換するメソッド(){
+		Translate trans = new Translate();
+		int number  = 28;
+		String eng = trans.translateEng(number);
+		assertThat("twenty eight",is(eng));
+	}
+	public void 数字29を英訳する変換するメソッド(){
+		Translate trans = new Translate();
+		int number  = 29;
+		String eng = trans.translateEng(number);
+		assertThat("twenty nine",is(eng));
+	}
+	public void 数字30を英訳する変換するメソッド(){
+		Translate trans = new Translate();
+		int number  = 30;
+		String eng = trans.translateEng(number);
+		assertThat("thirty",is(eng));
 	}
 }
--- src/pairpro/Translate.java
+++ src/pairpro/Translate.java
@@ -13,7 +13,37 @@ public class Translate {
 
     // 数値を英訳する変換するメソッド
     static String translateEng(int n) {
-    	String[] s = {"one","two","three","four","five"};
+    	String[] s = {"one",
+    			"two",
+    			"three",
+    			"four",
+    			"five",
+    			"six",
+    			"seven",
+    			"nine",
+    			"ten",
+    			"eleven",
+    			"twelve",
+    			"thirteen",
+    			"fourteen",
+    			"fifteen",
+    			"sixteen",
+    			"seventeen",
+    			"eighteen",
+    			"nineteen",
+    			"twenty",
+    			"twenty one",
+    			"twenty two",
+    			"twenty three",
+    			"twenty four",
+    			"twenty five",
+    			"twenty six",
+    			"twenty seven",
+    			"twenty eight",
+    			"twenty nine",
+    			"thirty"
+    			};
         return s[n-1];
+        
     }
 }
